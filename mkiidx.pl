64ae264e22eba555845c20bf79c5fd77282f8e3b
lmg <lorenz.gillner@hs-wismar.de>
2023-08-24 17:26:27 +0200

    Reformat code

--- mkiidx.pl
+++ mkiidx.pl
@@ -5,60 +5,63 @@ use warnings;
 use Text::CSV;
 
 sub deduplicate {
-	my @array_with_duplicates = @_;
+    my @array_with_duplicates = @_;
 
-	my @array_without_duplicates;
-	my %seen;
+    my @array_without_duplicates;
+    my %seen;
 
-	foreach my $element (@array_with_duplicates) {
-		unless ($seen{$element}) {
-			push @array_without_duplicates, $element;
-			$seen{$element} = 1;
-		}
-	}
+    foreach my $element (@array_with_duplicates) {
+        unless ( $seen{$element} ) {
+            push @array_without_duplicates, $element;
+            $seen{$element} = 1;
+        }
+    }
 
-	return @array_without_duplicates;
+    return @array_without_duplicates;
 }
 
-if (@ARGV != 1) {
-	die "Usage: $0 <database.csv>\n";
+if ( @ARGV != 1 ) {
+    die "Usage: $0 <database.csv>\n";
 }
 
 my $db = $ARGV[0];
 
 my %corpus;
 
-my $csv = Text::CSV->new({ binary => 1 }) or die "Cannot use CSV: " . Text::CSV->error_diag();
+my $csv = Text::CSV->new( { binary => 1 } )
+  or die "Cannot use CSV: " . Text::CSV->error_diag();
 
-open(my $f, '<', $db) or die "Could not open '$db': $!";
+open( my $f, '<', $db ) or die "Could not open '$db': $!";
 
-while (my $line = $csv->getline($f)) {
-	my $key = $line->[0];
-	my @tags = split(/;/, $line->[1]);
-	my $path = $line->[2];
-	my @words = split(' ', $line->[3]);
-	
-	# this is stupid, but sufficient for now
-	push @words, @tags;
+while ( my $line = $csv->getline($f) ) {
+    my $key   = $line->[0];
+    my @tags  = split( /;/, $line->[1] );
+    my $path  = $line->[2];
+    my @words = split( ' ', $line->[3] );
 
-	@words = deduplicate(@words);
+    # this is stupid, but sufficient for now
+    push @words, @tags;
 
-	while (my $word = shift @words) {
-		if (!$corpus{$word}) {
-			@{$corpus{$word}} = ($key);
-		} else {
-			push @{$corpus{$word}}, $key;
-		}
-	}
+    @words = deduplicate(@words);
+
+    while ( my $word = shift @words ) {
+        if ( !$corpus{$word} ) {
+            @{ $corpus{$word} } = ($key);
+        }
+        else {
+            push @{ $corpus{$word} }, $key;
+        }
+    }
 }
 
-if ($csv->eof) {
+if ( $csv->eof ) {
     close($f);
-} else {
+}
+else {
     $csv->error_diag();
 }
 
-foreach my $k (keys %corpus) {
-	print "$k,";
-	print join(';', @{$corpus{$k}}) . "\n";
+foreach my $k ( keys %corpus ) {
+    print "$k,";
+    print join( ';', @{ $corpus{$k} } ) . "\n";
 }
